95e641b701191834f555be1ab1b07f60ef23bc7e
Otire82 <a.jeanne01@sncf.fr>
2022-06-01 16:03:10 +0200

    js1

---
+++ site/js/portfolio.js
@@ -0,0 +1,8 @@
+/***************    JS    1    ***************/
+ let changeImage = document.querySelector('.portrait');
+ changeImage.addEventListener('click', 
+ function() {
+    changeImage.src = 'https://cdn.pixabay.com/photo/2015/11/13/20/15/light-bulb-1042480__340.jpg';
+    }
+ );
+ 
