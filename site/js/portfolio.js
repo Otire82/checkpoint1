4abd8b39a17a762edd25360f25270f8d697f80ce
Otire82 <a.jeanne01@sncf.fr>
2022-06-01 23:21:23 +0200

    JS2 sans couleur

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,4 +5,15 @@
     changeImage.src = 'https://cdn.pixabay.com/photo/2015/11/13/20/15/light-bulb-1042480__340.jpg';
     }
  );
- 
+ /***************    JS    2    ***************/
+
+let button = document.querySelector('#button')
+
+button.addEventListener('click', promptMe);
+function promptMe(){
+    let currentName = document.querySelector('#name')
+    let newName = prompt("Your name?");
+     currentName.innerText = newName;
+     //newName.style.color = 'white';
+};
+
